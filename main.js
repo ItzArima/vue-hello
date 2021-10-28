77dda2425cd4a285d3d56aa9eca86188f52d679a
ItzArima <skaypaprilia@gmail.com>
2021-10-28 17:33:48 +0200

    immagine inserita

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const root = new Vue ({
     el: '#root',
     data : {
-        Titolo :"Titolo con vue"
+        Titolo :"Titolo con vue",
+        immagine: "https://picsum.photos/200/300"
     }
 });
